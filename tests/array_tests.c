0dff32ffff2076f27a2c7dff0cd75091acdb9b13
Rodrigo Martin <rodrigoleonardomartin@gmail.com>
2019-02-28 16:31:40 -0300

    More tests

--- tests/array_tests.c
+++ tests/array_tests.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 #include "../src/array.h"
 #include "snow.h"
 
@@ -37,14 +39,84 @@ describe(arrays) {
         Array.destroy(&sut);
       }
     }
+
+    subdesc("Array::from") {
+      it("Creates the array copying the given elements") {
+        int* copy_int(int* payload) {
+          if (!payload) return null;
+
+          int* new_int = (int*) malloc(sizeof(int));
+
+          if (!new_int) return null;
+
+          *new_int = *payload;
+
+          return new_int;
+        }
+
+        int one = 1;
+        int two = 2;
+
+        int* primitive[] = { &one, &two };
+
+        array_t* sut = Array.from(copy_int, 2, primitive);
+
+        asserteq(sut->count, 2);
+        assertneq(sut->elements[0], &one, "Elements should have been copied!");
+        asserteq(*((int*) sut->elements[0]), one, "Elements should have been copied!");
+        assertneq(sut->elements[1], &two, "Elements should have been copied!");
+        asserteq(*((int*) sut->elements[1]), two, "Elements should have been copied!");
+
+        // Cleanup because we do not want to leak
+        Array.destroy_with_free(&sut);
+      }
+    }
+
+    subdesc("Array::with_capacity") {
+      it("Creates the array with the given capacity (should it be fixed?)") {
+        array_t* sut = Array.with_capacity(10);
+
+        asserteq(sut->capacity, 10);
+
+        Array.destroy(&sut);
+      } 
+    }
   }
 
   subdesc("Array destruction") {
     it("Array::destroy deallocates memory and nullifies the pointer") {
-      sut = Array.empty();
+      array_t* sut = Array.empty();
       Array.destroy(&sut);
 
       asserteq(sut, null);
+    }
+
+    it("Array::destroy_with_free destroys and calls free on each of the elements") {
+      int* something = (int*) malloc(sizeof(int));
+      int* primitive[] = { something };
+
+      array_t* sut = Array.of(1, primitive);
+      
+      Array.destroy_with_free(&sut);
+
+      asserteq(sut, null); // This does not assert nothing really. You should check valgrind output (0 bytes lost 😊)
+    }
+
+    it("Array::destroy_with_destructor calls the custom destructor") {
+      // Find a better way to mock?
+      boolean was_called = false;
+      void effectful_destructor(void* x) {
+        was_called = true;
+      }
+
+      int one = 1;
+      int* primitive[] = { &one };
+
+      array_t* sut = Array.of(1, primitive);
+
+      Array.destroy_with_destructor(&sut, effectful_destructor);
+
+      assert(was_called);
     }
   }
 }
